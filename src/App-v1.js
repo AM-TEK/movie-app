e1685112d3bccc266514284ef1c30873fc7680c2
Alejandro Maldonado <am@AM-WEBDEV.attlocal.net>
2024-02-16 14:37:15 -0600

    useEffect for data fetching

--- src/App-v1.js
+++ src/App-v1.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const tempMovieData = [
   {
@@ -50,26 +50,9 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-const KEY = '3d1ff800'
-
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
-  const [isLoading, setIsLoading] = useState(false)
-  const query = "interstellar"
-
-  useEffect(function() {
-    async function fetchMovies() {
-      setIsLoading(true)
-      const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`)
-      const data = await res.json()
-      setMovies(data.Search)
-      setIsLoading(false)
-    } 
-    fetchMovies()
-  }, [])
-
-  
 
   return (
     <>
@@ -79,7 +62,7 @@ export default function App() {
       </NavBar>
       <Main>
         <Box>
-          {isLoading ? <Loader /> : <MovieList movies={movies} />}
+          <MovieList movies={movies} />
         </Box>
         <Box>
            <WatchedSummary watched={watched} />
@@ -88,10 +71,6 @@ export default function App() {
       </Main>
     </>
   );
-}
-
-function Loader() {
-  return <p className="loader">L O A D I N G ...</p>
 }
 
 function NavBar({children}) {
